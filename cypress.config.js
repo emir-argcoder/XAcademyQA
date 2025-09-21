c893f7b824d989237c733b966042f1ab01952e4a
Grupo102025 <grupo10escmb.2025@gmail.com>
2025-09-21 15:37:32 -0300

    feat: Se agrega automatizacion para el alta de eventos

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "joejqs",
   e2e: {
-    video:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
