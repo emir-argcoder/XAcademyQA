d00efd43832dc8cc52d1f18bcb7a9a90351e1625
emir-argcoder <ea.samsam@gmail.com>
2025-09-22 22:49:13 -0300

    Test negativo para modificar o crear nuevas salas.

--- cypress/e2e/creadorSala/crearSala.cy.js
+++ cypress/e2e/creadorSala/crearSala.cy.js
@@ -1,5 +1,8 @@
 describe('Visualización Gestión de Salas en Main', () => {
     it('Logueo de credenciales usuario Cliente', () => {
+
+        cy.viewport(1280, 800)
+
         cy.visit('https://vps-3696213-x.dattaweb.com/auth/login')
 
         cy.get('[data-cy="input-email"]')
@@ -22,6 +25,16 @@ describe('Visualización Gestión de Salas en Main', () => {
         cy.url({ timeout: 10000 }).should('not.include', '/auth/login')
 
         // click en el menú "Gestión de Salas"
-        cy.contains('a, button, li', 'Salas').click()
+        cy.contains('Gestionar Salas')
+            .click()
+
+        cy.get('.container')
+            .first()
+            .should('be.visible')
+            .invoke('text')
+            .then((textoPantalla) => {
+                cy.log('Texto de la pantalla: ' + textoPantalla)
+                console.log('Texto de la pantalla: ' + textoPantalla)
+            })
     })
 })
